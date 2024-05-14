290df46aa5f74056b8015836577562289c50509d
AbhishekCS3459 <abhishekverman3459@gmail.com>
2024-05-14 23:18:49 +0530

    Added the files for the server

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['cdn1.iconfinder.com', 'cdn2.iconfinder.com'],
+    },
+};
+
+export default nextConfig;
